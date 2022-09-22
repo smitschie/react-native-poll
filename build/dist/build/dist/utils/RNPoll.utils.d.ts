efc4c77de207a17c36a363a5cd8563c549d3fdc9
smitchie <michella.saliby@gmail.com>
2022-09-22 19:46:53 +0200

    delet build

--- build/dist/build/dist/utils/RNPoll.utils.d.ts
+++
@@ -1,9 +0,0 @@
-export const __esModule: boolean;
-export function countPercentage(value: any, sum: any): number;
-export function convertPercentageString(percentage: any): string;
-export function calculateProgressBarAnimation({ percentage, hasBeenVoted, }: {
-    percentage: any;
-    hasBeenVoted: any;
-}): {
-    width: react_native_1.Animated.AnimatedInterpolation;
-};
--- build/dist/lib/utils/RNPoll.utils.d.ts
+++
@@ -1,7 +0,0 @@
-import { Animated } from "react-native";
-import { IRNPollItemProps } from "../components/RNPollItem";
-export declare const countPercentage: (value: number, sum: number) => number;
-export declare const convertPercentageString: (percentage: number) => string;
-export declare const calculateProgressBarAnimation: ({ percentage, hasBeenVoted, }: Pick<IRNPollItemProps, "percentage" | "hasBeenVoted">) => {
-    width: Animated.AnimatedInterpolation;
-};
